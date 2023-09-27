8a6c51a513649401fb62b2d595fc4a8be82af1aa
georgecatalin <george.calin@gmail.com>
2023-09-27 17:06:49 +0300

    understand database interaction: INSERT and SELECT

--- Django Models Databases Queries/django_models/office_app/models.py
+++ Django Models Databases Queries/django_models/office_app/models.py
@@ -6,3 +6,6 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name} is {self.age} old."
