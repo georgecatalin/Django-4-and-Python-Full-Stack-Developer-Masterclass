a5622c9e87750f64c41caeadf6e2f78f8a74bf9e
georgecatalin <george.calin@gmail.com>
2023-09-28 13:13:15 +0300

    understand updating models

--- Django Models Databases Queries/django_models/office_app/models.py
+++ Django Models Databases Queries/django_models/office_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(80)])
+    heartrate = models.IntegerField(default=45, validators=[MinValueValidator(30),MaxValueValidator(200)])
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name} is {self.age} old."
