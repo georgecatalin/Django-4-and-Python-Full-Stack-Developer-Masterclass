56e01cc937e875304d09a0d24e12ad3976114545
georgecatalin <george.calin@gmail.com>
2023-09-29 17:29:53 +0300

    Understand how to list, add, delete records in a model through a view

--- Django Admin/car_website/cars/models.py
+++ Django Admin/car_website/cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    # pk is added automatically by Django
+    brand = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+
+    def __str__(self):
+        return f"Car is {self.brand} and {self.year}."
--- Django Admin/car_website/cars/views.py
+++ Django Admin/car_website/cars/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
+from django.urls import reverse
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    my_context_dictionary = { 'all_cars_key': all_cars}
+
+
+    return render(request, 'cars/list.html', context= my_context_dictionary)
 
 
 def add(request):
-    return render(request, 'cars/add.html')
+    print(request.POST) # e.g. -> <QueryDict: {'csrfmiddlewaretoken': ['n9E2sEjoZRZNehLTQZUi50MgnpXCvCSaUMEwxBNHpzvz89Eg13zH6PwAxz7n1nHB'], 'brand': ['Renault'], 'year': ['2007']}>
+    if request.POST:
+        brand_value = request.POST['brand']
+        year_value = int(request.POST['year'])
+        models.Car.objects.create(brand=brand_value, year= year_value)
+        
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        print(request.POST)
+        primary_key = request.POST['record_id']
+        
+        try:
+            models.Car.objects.get(pk=primary_key).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('There is not such primary key in the database')
+            return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/delete.html')
