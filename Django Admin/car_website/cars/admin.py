85fbb86bd69279485fd0644bba53e146c76038e7
georgecatalin <george.calin@gmail.com>
2023-09-29 18:29:36 +0300

    Understand Django Admin interface, and registering models to Django Admin interface the easy and the hard way

--- Django Admin/car_website/cars/admin.py
+++ Django Admin/car_website/cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from cars.models import  Car
+
 
 # Register your models here.
+# admin.site.register(Car) ## --> this is the simple way to register the model to the Django Admin Interface
+
+class CarAdmin(admin.ModelAdmin):
+    # fields = ['year', 'brand']  # --> reorder the fields
+    fieldsets = [    
+        ('TIME INFORMATION', {'fields': ['year']}),
+        ('CAR INFORMATION', {'fields': ['brand']})
+    ]  # --> this way to structure the fields into groups
+
+admin.site.register(Car, CarAdmin)
+
+# read more here https://docs.djangoproject.com/en/4.2/ref/contrib/admin/
