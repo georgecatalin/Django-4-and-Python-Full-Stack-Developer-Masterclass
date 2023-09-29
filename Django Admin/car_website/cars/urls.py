6d485aa7a1346eb821e9f7b0e030d12b4b01853b
georgecatalin <george.calin@gmail.com>
2023-09-29 14:25:47 +0300

    create templates and views for a demo website

---
+++ Django Admin/car_website/cars/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'cars'
+
+urlpatterns = [
+     path('list/', views.list, name='list'),
+     path('add/', views.add, name = 'add'),
+     path('delete', views.delete, name = 'delete')
+]
---
+++ Django Admin/car_website/cars/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def list(request):
+    return render(request, 'cars/list.html')
+
+
+def add(request):
+    return render(request, 'cars/add.html')
+
+
+def delete(request):
+    return render(request, 'cars/delete.html')
