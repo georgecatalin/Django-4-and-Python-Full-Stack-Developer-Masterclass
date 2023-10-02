c111c42c51937b87abbff6ce7f0487b01d508b77
georgecatalin <george.calin@gmail.com>
2023-10-02 18:00:52 +0300

    understand widgets and styling with Django Forms

--- Django Forms/car_rental_website/rentals/forms.py
+++ Django Forms/car_rental_website/rentals/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class RentalReview(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
+    first_name = forms.CharField(label='First Name', max_length=100, widget=forms.TextInput(attrs={'class':'myform'}))
     last_name = forms.CharField(label='Last Name', max_length=100)
     email_address = forms.EmailField(label='Email Address')
-    feedback = forms.CharField(label='Please enter your review here:')
+    feedback = forms.CharField(label='Please enter your review here:', widget=forms.Textarea(attrs={'class' : 'myform', 'rows' : '2'}))
